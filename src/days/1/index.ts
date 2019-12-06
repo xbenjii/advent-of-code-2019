a113947c699ca639888818c452f5cba9e25f4180
Ben Fortune <ben@prestigegifting.co.uk>
2019-12-06 14:39:23 +0000

    day 1 stage 2

--- src/days/1/index.ts
+++ src/days/1/index.ts
@@ -1,15 +1,35 @@
-async function preprocess(input: string) {
+function preprocess(input: string) {
     return input.split(/\n/).filter(Boolean).map(Number);
 }
 
-async function stage1(data: number[]) {
+function calculateFuel(n: number) {
+    return Math.floor(n / 3) - 2;
+}
+
+function calculateFuelRecursive(n: number, total: number = 0): number {
+    const f = calculateFuel(n);
+    if(f > 0) {
+        return calculateFuelRecursive(f, total + f);
+    }
+    return total;
+}
+
+function stage1(data: number[]) {
     return data.reduce((a, n) => {
-        a += (Math.floor(n / 3) - 2);
+        a += calculateFuel(n);
+        return a;
+    }, 0);
+}
+
+function stage2(data: number[]) {
+    return data.reduce((a, n) => {
+        a += calculateFuelRecursive(n);
         return a;
     }, 0);
 }
 
 export async function run(input: string) {
-    const data = await preprocess(input);
-    console.log(await stage1(data));
+    const data = preprocess(input);
+    console.log(stage1(data));
+    console.log(stage2(data));
 }
